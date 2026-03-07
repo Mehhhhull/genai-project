f048f6ab210e1bef832fb209aea1524500a32e6a
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-07 19:48:27 +0530

    Register controller

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,13 +1,70 @@
-const userModel=require("../models/user.model")
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /**
  * @name registerUserController
  * @description Register a new user,expects username,email and password in the request body
  * @access Public
+*/
+async function registerUserController(req, res) {
+  const { username, email, password } = req.body;
 
- */
-async function registerUserController(req,res){}
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "Please provide username,email and password",
+    });
+  }
 
-module.exports={
-  registerUserController
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (isUserAlreadyExists) {
+    /*more complex check : isUserAlreadyExists.username==username*/
+    return res.status(400).json({
+      message: "User with the same username or email already exists",
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user:{
+      id:user._id,
+      username:user.username,
+      email:user.email
+    }
+  })
 }
+
+
+/**
+ * @name loginUserController
+ * @description Login a user,expects email and password in the request body
+ * @access Public
+*/
+async function loginUserController(req,res){}
+
+module.exports = {
+  registerUserController,
+};

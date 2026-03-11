1f475caa7b36e1c4afc888fa9c0347393d94d735
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-11 21:13:13 +0530

    generateInterviewReportController

--- server/src/controllers/interview.controllers.js
+++ server/src/controllers/interview.controllers.js
@@ -1,8 +1,9 @@
 const pdfParse=require("pdf-parse")
 const generateInterviewReport=require("../services/ai.service")
+const interviewReportModel=require("../models/interviewReport.model")
 
 async function generateInterviewReportController(req,res){
-  const resumeFile=req.file
+
 
   const resumeContent=pdfParse(req.file.buffer)
   const {selfDescription,jobDescription}=req.body
@@ -11,6 +12,18 @@ async function generateInterviewReportController(req,res){
     resume: resumeContent,
     selfDescription,
     jobDescription
+})
+
+const interviewReport=await interviewReportModel.create({
+  user:req.user._id,
+  resume:resumeContent,
+  selfDescription,
+  jobDescription,
+  ...interviewReportByAi
+})
+
+res.status(201).json({
+  message:"Interview Report Generated SUccessfully"
 })
 }
 

e6792831e2ffe940af33c7028156e9a3f36baed0
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-28 21:07:21 +0530

    Hook

--- client/src/features/auth/auth.context.jsx
+++ client/src/features/auth/auth.context.jsx
@@ -8,4 +8,10 @@ export const AuthProvider=({children})=>{
   const [user, setuser] = useState(null)
   const [loading, setloading] = useState(false)
 
+  return(
+    <AuthContext.Provider value={{user,setuser,loading,setloading}}>
+      {children}
+    </AuthContext.Provider>
+  )
+
 }
---
+++ client/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,31 @@
+import { use, useContext } from "react";
+import { AuthContext } from "../auth.context.jsx";
+import {login,register,logout,getMe} from "../auth.service.js"
+
+export const useAuth=()=>{
+  const context=useContext(AuthContext)
+  const {user,setUser,loading,setLoading}=context
+
+  const handleLogin=async ({email,password})=>{
+    setLoading(true)
+    const data=await login({email,password})
+    setUser(data.user)
+    setLoading(false)
+  }
+
+  const handleRegister=async ({name,email,password})=>{
+    setLoading(true)
+    const data=await register({name,email,password})
+    setUser(data.user)
+    setLoading(false)
+  }
+
+  const handleLogout=async ()=>{
+    setLoading(true)
+    await logout()
+    setUser(null)
+    setLoading(false)
+  }
+
+  return { user, loading, handleLogin, handleRegister, handleLogout };
+}

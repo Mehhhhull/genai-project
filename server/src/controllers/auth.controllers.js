1f7b6523694674fb1dd9ff56e4c7c3b123086a92
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-05-09 22:06:43 +0530

    Updted Cookies

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -116,7 +116,7 @@ async function logoutUserController(req,res){
     await tokenBlacklistModel.create({token})
   }
 
-  res.clearCookie("token")
+  res.clearCookie("token", { httpOnly: true, secure: true, sameSite: 'none' });
 
   res.status(200).json({
     message:"User Logged Out SUccessfully"
